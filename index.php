5235b49ebc6ac4a45582dfd557bf3f19b6ae88db
Lubu-J <124082145+Lubu-J@users.noreply.github.com>
2024-02-14 19:14:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 	require_once "config/config.php";
 	require_once "core/routes.php";
 	require_once "config/database.php";
-	require_once "Controllers/Alumnos.php";	
-	require_once "Controllers/Sedes.php";
 	require_once "Controllers/Usuarios.php";
-	require_once "Controllers/Carrera.php";
-	require_once "Controllers/Proceso.php";
-	require_once "Controllers/Escolars.php";
-	require_once "Controllers/Registro.php";
-	require_once "Controllers/vinculacion.php";
-	require_once "Controllers/Rcontraseña.php";
-	require_once "Controllers/Estadisticos.php";
 
 	
 	//Mediante la url vamos a saber que controlador se usa
