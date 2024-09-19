4448e451762808f06bf8958022807380f5f0b3cb
AL3XROO37 <javier.rosas.orosco@gmail.com>
2024-09-18 20:58:52 -0600

    Diseño

--- Config/database.php
+++ Config/database.php
@@ -1,32 +1,7 @@
 <?php
-
 class Conectar
 {
-
 	public static function conexion()
-	{
-
-		$servidor = "127.0.0.1"; // Dirección del servidor de la base de datos
-		$usuario = "root"; // Nombre de usuario de la base de datos
-		$contraseña = "tiburon2014"; // Contraseña de la base de datos (deja en blanco si no tiene contraseña)
-		$baseDeDatos = "wido"; // Nombre de la base de datos
-
-		// Crear y devolver la conexión
-		$conexion = new mysqli($servidor, $usuario, $contraseña, $baseDeDatos);
-
-		// Verificar si hay errores en la conexión
-		if ($conexion->connect_error) {
-			die("Error de conexión: " . $conexion->connect_error);
-		}
-
-		return $conexion;
-	}
-}
-
-
-
-/*
-public static function conexion()
 	{
 
 		$servidor = "localhost"; // Dirección del servidor de la base de datos
@@ -44,4 +19,4 @@ public static function conexion()
 
 		return $conexion;
 	}
-		*/
+}
